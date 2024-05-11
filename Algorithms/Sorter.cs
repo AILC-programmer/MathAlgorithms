16cbc23b117b8ad8efb9a38b6f13b8d833b54180
Arman Sadeghi <113186227+AILC-programmer@users.noreply.github.com>
2024-05-11 10:22:26 +0330

    Initialized project.

---
+++ Algorithms/Sorter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    public class Sorter
+    {
+        public double[] BubbleSort(params double[] numbers)
+        {
+
+
+            return new double[0];
+        }
+    }
+}
